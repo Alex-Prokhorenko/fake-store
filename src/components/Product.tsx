d9e5d89e168c6b86a3bbc581dbeb65c2eb8a46b5
Alex-Prokhorenko <galexthunder@gmail.com>
2022-12-22 16:42:52 +0700

    feat: product.tsx now displays the image, price, and description of the product. It is possible to hide or show the description by clicking on the button.

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,13 +1,26 @@
 import {productModel} from "../models/models";
+import {useState} from "react";
 
 interface productProps {
     product: productModel
 }
 
 const Product = ({product}: productProps) => {
+
+    const[description, setDescription] = useState(false);
+    const handleDescription = () => {
+        setDescription(!description);
+    }
+
     return (
-        <div className='w-1/2 mx-auto border border-stone-900'>
+        <div className='p-3 w-1/2 mx-auto border border-stone-900 flex flex-col items-center'>
+            <img src={product.image} alt={product.title} className='w-1/6'/>
             <p>{product.title}</p>
+            <p className='font-bold'>{product.price}</p>
+            <button onClick={handleDescription}
+                    className='bg-red-900 text-white p-1'>{description ? 'Hide description' : 'Show description'}
+            </button>
+            {description && <p>{product.description}</p>}
         </div>
     )
 }
