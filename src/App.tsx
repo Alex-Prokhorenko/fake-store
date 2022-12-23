ccd5cfd48b75d689d1103f543e28c6054fc69bce
Alex-Prokhorenko <galexthunder@gmail.com>
2022-12-23 15:54:20 +0700

    fix: products are now displayed using map function

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import Product from './components/Product';
-import {product} from './data/storage';
+import {products} from './data/storage';
 
 function App() {
-  return (
-    <div className='App'>
-      <Product product={product[0]}/>
-    </div>
-  );
+    return (
+        <div className='App'>
+            {products.map( product => <Product product={product}/> )}
+        </div>
+    );
 }
 
 export default App;
