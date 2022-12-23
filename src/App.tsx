acd99795126ca61ae810e9029d9c8cf3eeb4ba3f
Alex-Prokhorenko <galexthunder@gmail.com>
2022-12-23 16:34:59 +0700

    feat: added axios library. now the products are downloaded from fakestoreapi.com

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Product from './components/Product';
-import {products} from './data/storage';
+import axios from "axios";
+import {productModel} from "./models/models";
+
 
 function App() {
+
+    const[products, setProducts] = useState<productModel[]>([]);
+    async function fetchProducts() {
+        const response = await axios.get<productModel[]>('https://fakestoreapi.com/products?limit=5');
+        setProducts(response.data);
+
+    }
+    useEffect(() =>{
+        fetchProducts();
+    }, []);
     return (
         <div className='App'>
             {products.map( product => <Product key={product.id} product={product}/> )}
